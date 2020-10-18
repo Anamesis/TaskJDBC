f844edd41d1dcc0b8333ea0c9f09e62b5fb98633
Anamesis <ivan6utakov@gmail.com>
2020-10-18 18:31:33 +0500

    CHange Service

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,43 @@
 package jm.task.core.jdbc.util;
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private String URL =
+    public static void main (String[] args){
+        Connection connection;
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+//    public static Connection getConnection() throws SQLException {
+//        System.out.println("nn***** MySQL JDBC Connection Testing *****");
+//        Connection conn = null;
+//        try {
+//            Class.forName("com.MySQL.jdbc.Driver").newInstance();
+//            String userName = "root";
+//            String password = "123";
+//            String url = "jdbc:MySQL://localhost:3306/Users1";
+//            conn = DriverManager.getConnection(url, userName, password);
+//            System.out.println("Database Connection Established...");
+//        } catch (Exception ex) {
+//            System.err.println("Cannot connect to database server");
+//            ex.printStackTrace();
+//        } finally {
+//            if (conn != null) {
+//                try {
+//                    System.out.println("n***** Let terminate the Connection *****");
+//                    conn.close();
+//                    System.out.println("Database connection terminated... ");
+//                } catch (Exception ex) {
+//                    System.out.println("Error in connection termination!");
+//                }
+//            }
+//        }
+//        return conn;
+//    }
 }
