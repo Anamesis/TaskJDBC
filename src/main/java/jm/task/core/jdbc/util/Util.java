167394f8479c931e0f9e3526606e651589671da4
Anamesis <ivan6utakov@gmail.com>
2020-10-19 14:52:37 +0500

    CHange Service

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,40 +4,40 @@ import com.mysql.fabric.jdbc.FabricMySQLDriver;
 import java.sql.*;
 
 public class Util {
-    private String URL =
-    public static void main (String[] args){
-        Connection connection;
-        try {
-            Driver driver = new FabricMySQLDriver();
-            DriverManager.registerDriver(driver);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-//    public static Connection getConnection() throws SQLException {
-//        System.out.println("nn***** MySQL JDBC Connection Testing *****");
-//        Connection conn = null;
+//    private String URL =
+//    public static void main (String[] args){
+//        Connection connection;
 //        try {
-//            Class.forName("com.MySQL.jdbc.Driver").newInstance();
-//            String userName = "root";
-//            String password = "123";
-//            String url = "jdbc:MySQL://localhost:3306/Users1";
-//            conn = DriverManager.getConnection(url, userName, password);
-//            System.out.println("Database Connection Established...");
-//        } catch (Exception ex) {
-//            System.err.println("Cannot connect to database server");
-//            ex.printStackTrace();
-//        } finally {
-//            if (conn != null) {
-//                try {
-//                    System.out.println("n***** Let terminate the Connection *****");
-//                    conn.close();
-//                    System.out.println("Database connection terminated... ");
-//                } catch (Exception ex) {
-//                    System.out.println("Error in connection termination!");
-//                }
-//            }
+//            Driver driver = new FabricMySQLDriver();
+//            DriverManager.registerDriver(driver);
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
 //        }
-//        return conn;
 //    }
+    public static Connection getConnection() throws SQLException {
+        System.out.println("nn***** MySQL JDBC Connection Testing *****");
+        Connection conn = null;
+        try {
+            Class.forName("com.MySQL.jdbc.Driver").newInstance();
+            String userName = "root";
+            String password = "123";
+            String url = "jdbc:MySQL://localhost:3306/Users1";
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("Database Connection Established...");
+        } catch (Exception ex) {
+            System.err.println("Cannot connect to database server");
+            ex.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    System.out.println("n***** Let terminate the Connection *****");
+                    conn.close();
+                    System.out.println("Database connection terminated... ");
+                } catch (Exception ex) {
+                    System.out.println("Error in connection termination!");
+                }
+            }
+        }
+        return conn;
+    }
 }
