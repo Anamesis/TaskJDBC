93097fc6d2bab8eeafcc9dfc02944d7c1045a711
Anamesis <ivan6utakov@gmail.com>
2020-10-19 22:39:35 +0500

    Final Version

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            String userName = "root";
+            String password = "123";
+            String url = "jdbc:mysql://localhost:3306/Users1";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, userName, password);
+            System.out.println("Database connection..");
+        } catch (Exception e) {
+            System.out.println("Connection exception");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
